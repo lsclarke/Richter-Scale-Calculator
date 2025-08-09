7173d865453968e40112d3f265a354151d6a515e
lsclarke <57269847+lsclarke@users.noreply.github.com>
2025-08-09 13:30:45 -0400

    New namespace rsc (RichterScaleCalculator)

--- CalculateRichterScale/CalculateRichterScale/CalculateRichterScale.cpp
+++ CalculateRichterScale/CalculateRichterScale/CalculateRichterScale.cpp
@@ -6,32 +6,21 @@
 #include <cmath>
 
 class Earthquake {
-
-private:
-    int _id;
-    int nextID = 0;
 public:
-    Earthquake(double value)
-    {
-        _id = ++nextID;
-        magnitude = value;
-    }
+    Earthquake(double value) { magnitude = value; }
 
-    int returnID() { return _id; }
     double magnitude{};
     double intensity{};
     double energy{};
-
+    Earthquake* quake;
 };
 
-class RichterCalculator {
-public:
-
+//Richter Scale Calculator
+namespace rsc {
     /// <summary>
     /// Responsible for calculating the magnitude of earthquakes!
     /// </summary>
     double CalculateEnergy(Earthquake* quake) {
-
         quake->energy = (2.5 * pow(10, 4)) * pow(10, (1.5 * quake->magnitude));
         return quake->energy;
     }
@@ -39,15 +28,12 @@ public:
     /// <summary>
     /// Responsible for calculating the difference in intensity between two earthquakes!
     /// </summary>
-
     double CalculateIntensityDifference(Earthquake* quake1, Earthquake* quake2) {
 
         double magnitude_Difference = (quake1->magnitude - quake2->magnitude);
-
         double diffIntensity = round(pow(10, magnitude_Difference));
 
         quake1->intensity = quake2->intensity = diffIntensity / 2;
-
         if (quake1->magnitude > quake2->magnitude)
             std::cout << "Earthquake #1 Intensity:: " << quake1->magnitude << " was approximately " << diffIntensity << " times stronger than Earthquake #2 Intensity:: " << quake2->magnitude << std::endl;
 
@@ -60,7 +46,6 @@ public:
 
 int main()
 {
-    //Earthquake Class Instance Objects
     Earthquake* quake1 = new Earthquake(9.3);
     Earthquake* quake2 = new Earthquake(8.7);
 
@@ -69,16 +54,12 @@ int main()
 
     Earthquake* quakeEnergy = new Earthquake(9.3);
 
-    //Ricther Class Instance Object
-    RichterCalculator* scale = new RichterCalculator;
+    rsc::CalculateIntensityDifference(quake1, quake2);
+    rsc::CalculateIntensityDifference(quake3, quake4);
 
-    //Calculate the intensity difference
-    scale->CalculateIntensityDifference(quake1, quake2);
-    scale->CalculateIntensityDifference(quake3, quake4);
-
-    //String output of energy from earthquake 3
-    std::cout << "Earthquake ID#" << quake3->returnID() << " Energy Output:: " << scale->CalculateEnergy(quake3) << std::endl;
+    std::cout << "Energy Output:: " << rsc::CalculateEnergy(quake3) << std::endl;
 
     std::cin.get();
     return 0;
 }
+
